afb6e275a465686064d90cef30f8c0f273a8be11
Sergii Pavlenko <sm.pavlenko@gmail.com>
2018-01-11 18:34:00 +0100

    Roughly implemented document service

--- src/main/kotlin/com/translator/service/DocumentService.kt
+++ src/main/kotlin/com/translator/service/DocumentService.kt
@@ -1,6 +1,15 @@
 package com.translator.service
 
+import org.apache.http.HttpResponse
+import org.apache.http.client.HttpClient
+import org.apache.http.NameValuePair
+import org.apache.http.client.entity.UrlEncodedFormEntity
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.http.message.BasicNameValuePair
 import org.springframework.stereotype.Service
+import java.io.ByteArrayOutputStream
+import java.util.*
 
 /**
  * @author Sergii Pavlenko
@@ -18,8 +27,23 @@ interface DocumentService {
 object DocumentServiceImpl : DocumentService {
 
     override fun translate(from: String, to: String, text: String): String {
-        // TODO
-        return ""
+
+        val httpPost = HttpPost("https://translate.googleapis.com/translate_a/single")
+        val nvps = ArrayList<NameValuePair>()
+        nvps.add(BasicNameValuePair("client", "gtx"))
+        nvps.add(BasicNameValuePair("sl", "en"))
+        nvps.add(BasicNameValuePair("tl", "de"))
+        nvps.add(BasicNameValuePair("dt", "t"))
+        nvps.add(BasicNameValuePair("q", text))
+        httpPost.entity = UrlEncodedFormEntity(nvps)
+
+        val httpClient: HttpClient = HttpClientBuilder.create().build()
+        val response: HttpResponse = httpClient.execute(httpPost)
+
+        val out = ByteArrayOutputStream()
+        response.entity.writeTo(out)
+        val responseString = out.toString()
+
+        return responseString
     }
 }
-
