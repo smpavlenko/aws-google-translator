72fc0f3e4d00a3ba9ad6dee45fb2c545a767f692
Sergii Pavlenko <sm.pavlenko@gmail.com>
2018-03-31 10:57:32 +0200

    Updated local installation script

--- prepare_local_env.sh
+++ prepare_local_env.sh
@@ -2,3 +2,8 @@
 
 # Install Packer locally (macOS 10.13.3)
 brew install packer
+
+# Install AWS CLI
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
