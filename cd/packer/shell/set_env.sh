8ff613b0ea2616bee969d374051af17ced32365d
sergii.pavlenko <sm.pavlenko@gmail.com>
2018-04-02 14:38:22 +0200

    Packer: added recognition of Subnet id

--- cd/packer/shell/set_env.sh
+++ cd/packer/shell/set_env.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 vpc_id=$(aws ec2 describe-vpcs --filters Name=isDefault,Values=false --output json | jq -r .Vpcs[0].VpcId | tr -d '\040\011\012\015')
+subnet_id=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpc_id --output json | jq -r .Subnets[0].SubnetId | tr -d '\040\011\012\015')
+
 sudo echo "vpc_id: $vpc_id" >> /etc/environment
+sudo echo "subnet_id: $subnet_id" >> /etc/environment
