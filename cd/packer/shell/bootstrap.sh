0b0a208877cc9e9045e93d21ce5c28781486e62d
sergii.pavlenko <sm.pavlenko@gmail.com>
2018-04-02 14:28:33 +0200

    Packer: added installation of awscli

--- cd/packer/shell/bootstrap.sh
+++ cd/packer/shell/bootstrap.sh
@@ -3,3 +3,4 @@
 sudo apt-get update
 sudo apt-get -y install sudo
 sudo apt-get -y install ansible
+sudo apt-get install awscli
--- cd/packer/shell/cleanup.sh
+++ cd/packer/shell/cleanup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Shell provisioning script to clean up at the end
-# 1. remove ansible
-# 2. cleanup and remove unused packages
-# 3. remove finger prints of keys
-# 4. remove tmp
+# 1. remove awscli
+# 2. remove ansible
+# 3. cleanup and remove unused packages
+# 4. remove finger prints of keys
+# 5. remove tmp
+sudo apt-get remove -y awscli
 sudo apt-get remove -y ansible
 sudo apt-get autoclean -y
 sudo apt-get autoremove -y
