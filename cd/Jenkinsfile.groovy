68c866baf8c51fb820e9a2188af1e726397647f7
sergii.pavlenko <sm.pavlenko@gmail.com>
2018-04-21 14:38:27 +0200

    Jenkinsfile: Added required variables

--- cd/Jenkinsfile.groovy
+++ cd/Jenkinsfile.groovy
@@ -18,6 +18,12 @@ pipeline {
                 script {
                     if (params.AWS_ACCOUNT == null || params.AWS_ACCOUNT == "") {
                         error("Can't proceed without AWS Account Id")
+                    } else if (params.REGION == null || params.REGION == "") {
+                        error("Can't proceed without REGION")
+                    } else if (params.CF_ROLE == null || params.CF_ROLE == "") {
+                        error("Can't proceed without CloudFormation IAM role")
+                    } else if (params.JENKINS_ROLE == null || params.JENKINS_ROLE == "") {
+                        error("Can't proceed without Jenkins IAM role")
                     }
                 }
             }
@@ -35,6 +41,25 @@ pipeline {
             }
         }
 
+        stage('Set required environment variables') {
+            steps {
+                script {
+
+                    env.VPC_PARAMS_PATH = "cd/cloudformation/vpc/${REGION}/vpc_params.yaml"
+                    env.S3_PARAMS_PATH = "cd/cloudformation/s3/${REGION}/s3_params.yaml"
+                    env.ELB_PARAMS_PATH = "cd/cloudformation/elb/${REGION}/elb_params.yaml"
+                    env.ASG_PARAMS_PATH = "cd/cloudformation/asg/${REGION}/asg_params.yaml"
+
+                    env.VPC_TEMPLATE_PATH = "cd/cloudformation/vpc/vpc_cfn.yaml"
+                    env.S3_TEMPLATE_PATH = "cd/cloudformation/s3/s3_cfn.yaml"
+                    env.ELB_TEMPLATE_PATH = "cd/cloudformation/elb/elb_cfn.yaml"
+                    env.ASG_TEMPLATE_PATH = "cd/cloudformation/asg/asg_cfn.yaml"
+
+                    env.AWS_PARTITION = "aws"
+                    env.CF_ROLE = "arn:${AWS_PARTITION}:iam::${AWS_ACCOUNT}:role/${CF_ROLE}"
+                }
+            }
+        }
 
     }
 
